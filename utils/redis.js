377a50d10f4cab1985097fb47c8f2b16524c82f1
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-21 18:11:05 +0300

    Add redis utility

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import { createClient } from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    // Handles connect event
+    this.client.on('connect', () => {
+      console.log('Connected to Redis');
+    });
+    // Handles error event
+    this.client.on('error', (err) => {
+      console.log("Redis connection error", err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+      return await this.client.get(key);
+    }
+
+  async set(key, value, duration) {
+    return await this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
