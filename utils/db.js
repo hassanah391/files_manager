e292fcb9e0f2b23803cb2771d9c6b475ca9501c9
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-21 23:04:43 +0300

    Add Mongodb utility

---
+++ utils/db.js
@@ -0,0 +1,39 @@
+import mongodb from 'mongodb';
+import config from '../config.js';
+
+const { MongoClient } = mongodb;
+
+class DBClient {
+  constructor() {
+    this.client = null;
+    this.db = null;
+    this.isConnected = false;
+    MongoClient.connect(config.db.url, { useUnifiedTopology: true }, (err, client) => {
+      if (err) {
+        console.error('MongoDB connection error:', err);
+        return;
+      }
+      this.client = client;
+      this.db = client.db(config.db.name);
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async nbUsers() {
+    if (!this.isConnected || !this.db) return 0;
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.isConnected || !this.db) return 0;
+    return this.db.collection('files').countDocuments();
+  }
+
+}
+
+const dbClient = new DBClient();
+export default dbClient;
