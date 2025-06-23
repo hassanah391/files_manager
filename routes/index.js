b61f29c2d09b54fda4888839b9f5fc2c3eb2b7a0
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-24 00:08:36 +0300

    Add a new route for /users

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@
 const express = require('express');
 const router = express.Router();
 const { getStatus, getStats } = require('../controllers/AppController');
+const { postNew } = require('../controllers/UsersController');
+
 
 // Middleware to log the time of each request
 const timeLog = (req, res, next) => {
@@ -14,14 +16,27 @@ const timeLog = (req, res, next) => {
 };
 router.use(timeLog);
 
+
 // GET /status - Returns Redis and DB status
 router.get('/status', async (req, res) => {
   res.send(await getStatus());
 });
 
+
 // GET /stats - Returns number of users and files in the database
 router.get('/stats', async (req, res) => {
   res.send(await getStats());
 });
+
+
+router.post('/users/', async (req, res) => {
+  const { email, password } = req.body;
+  const result = await postNew(email, password);
+  if (result && result.error) {
+    return res.status(400).json({ error: result.error });
+  }
+  return res.status(201).json(result);
+});
+
 
 module.exports = { router };
