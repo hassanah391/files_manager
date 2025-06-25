a44d1236688f4055b6e461584ecb4f697dfd4109
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-06-25 06:16:54 +0300

    Makes all logic in one folder controllers

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,37 @@
-// routes/index.js
-// Defines all API endpoints for the ALX Files Manager
-// - GET /status: Returns Redis and DB status
-// - GET /stats: Returns user and file counts
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
-const express = require('express');
-const router = express.Router();
-const { getStatus, getStats } = require('../controllers/AppController');
-const { postNew, getMe } = require('../controllers/UsersController');
-const { getConnect, getDisconnect } = require('../controllers/AuthController');
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
 
 
-// Middleware to log the time of each request
-const timeLog = (req, res, next) => {
-  console.log('Time: ', new Date().toISOString());
-  next();
-};
-router.use(timeLog);
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
 
 
-// GET /status - Returns Redis and DB status
-router.get('/status', async (req, res) => {
-  res.send(await getStatus());
-});
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
+  });
 
 
-// GET /stats - Returns number of users and files in the database
-router.get('/stats', async (req, res) => {
-  res.send(await getStats());
-});
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
 
-
-router.post('/users/', async (req, res) => {
-  const { email, password } = req.body;
-  const result = await postNew(email, password);
-  if (result && result.error) {
-    return res.status(400).json({ error: result.error });
-  }
-  return res.status(201).json(result);
-});
-
-
-router.get('/connect', async (req, res) => {
-  await getConnect(req, res);
-});
-
-
-router.get('/disconnect', async (req, res) => {
-  await getDisconnect(req, res);
-});
-
-
-router.get('/users/me', async (req, res) => {
-  await getMe(req, res);
-});
-
-module.exports = { router };
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+}
+export default controllerRouting;
